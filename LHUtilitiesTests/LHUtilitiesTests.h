b4ce9ee091c5708bbc97bd87f81ec7d541c31f6a
xcode <lt@LiHong-2.local>
2013-06-04 13:02:33 +0800

    Initial Commit

---
+++ LHUtilitiesTests/LHUtilitiesTests.h
@@ -0,0 +1,13 @@
+//
+//  LHUtilitiesTests.h
+//  LHUtilitiesTests
+//
+//  Created by xcode on 13-6-4.
+//  Copyright (c) 2013年 xcode. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LHUtilitiesTests : SenTestCase
+
+@end
---
+++ LHUtilitiesTests/LHUtilitiesTests.m
@@ -0,0 +1,32 @@
+//
+//  LHUtilitiesTests.m
+//  LHUtilitiesTests
+//
+//  Created by xcode on 13-6-4.
+//  Copyright (c) 2013年 xcode. All rights reserved.
+//
+
+#import "LHUtilitiesTests.h"
+
+@implementation LHUtilitiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LHUtilitiesTests");
+}
+
+@end
